7688e8a723b50ad2db38a48f76795bba41f8e661
Ashley Davis <ashley@codecapers.com.au>
2021-06-03 19:12:01 +1000

    Added express which will make for a better demo. In the future migth want to split this into seperate Node.js and Express plugins.

--- create-template/assets/src/index.js
+++ create-template/assets/src/index.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
 
-console.log("Hello world!");
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
